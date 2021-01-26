6e0de6d96b36db13d080093a2f748c75993231cb
Yoon Hee-Ku <heegu0311@naver.com>
2021-01-26 21:14:41 +0900

    9. 여러개의 input 상태 관리하기

--- wrs/src/InputSample.js
+++ wrs/src/InputSample.js
@@ -1,26 +1,37 @@
 import React, { useState } from 'react';
 
 function InputSample() {
-  const [text, setText] = useState('');
+  const [inputs, setInputs] = useState ({
+    name: '',
+    nickname: ''
+  });
+
+  const { name, nickname } = inputs;
 
   const onChange = (e) => {
-    setText(e.target.value);
+    const { value, name } = e.target;
+    const keyName = name;                 // 불필요한 한줄이지만 헷갈려서 이렇게도 적어봄
+    setInputs({
+      ...inputs,
+      [keyName] : value                   // keyName은 'name' 또는 'nickname'으로 사용하게됨
+    });
   };
-  
-//   const onChange = () => {      // 이건 왜 동작을 안할까?? e를 제거함.
-//     setText(this.value);        // e제거하니 target이 undefined error가 나서 this 사용 해봄. 역시 에러. this는 무조건 객체를 지칭하게되는지... 
-//   };
 
-  const onReset = (e) => {          // 어떤 것은 event를 써줘야 하고 위에서는 안쓰고. 차이를 모르겠다. e를 써줘도 동작은 제대로 되는데, 항상 써주면 안되는건가?
-    setText('');
+  const onReset = (e) => {
+    setInputs ({
+      name: '',
+      nickname: ''
+    });
   };
 
   return (
     <div>
-      <input onChange={onChange} value={text}  />
-      <button onClick={onReset}>초기화</button>
       <div>
-        <b>값: {text}</b>
+        <input name = "name" placeholder="이름" onChange={onChange} value={name}/>
+        <input name = "nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
+        <button onClick={onReset}>초기화</button>
+        <b>값: </b>
+        {name} ({nickname})
       </div>
     </div>
   );
