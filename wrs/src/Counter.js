08d147a995e39789a88c659a3502cd52ba5256f3
Yoon Hee-Ku <heegu0311@naver.com>
2021-01-30 22:54:00 +0900

    16. useEffect 마운트/언마운트/업데이트시 작업 설정
    useEffect 너무 어려움
    
    컴포넌트 마운트시, 어떤 작업
    컴포넌트 언마운트시, 어떤 작업
    컴포넌트 업데이트시, 어떤 작업을
    시킨다는 거는 알겠는데... 왜 코딩을 저렇게 하는건지.. deps가 무엇인지.. 모르겠음

--- wrs/src/Counter.js
+++ wrs/src/Counter.js
@@ -10,7 +10,7 @@ function Counter() {
     }
 
     const onDecrease = () => {
-        setNumber(prevNumber => prevNumber + 1);
+        setNumber(prevNumber => prevNumber - 1);
         console.log (-1);
     }
 
@@ -18,8 +18,8 @@ function Counter() {
         <div>
         <h1>{number}</h1>
         <button onClick={() => setNumber(number+1)}>+1</button>     {/*  // 동작 */}
-        {/* <button onClick={setNumber(number+1)}>+1</button>           // 미동작
-        <button onClick={onIncrease}>+1</button>                    // 동작 */}
+        <button onClick={setNumber(number+1)}>+1</button>
+        <button onClick={onIncrease}>+1</button>                    
         <button onClick={onDecrease}>-1</button>
         </div>
   );
