5174552fed03760f857d32d7fce1e08fecca806e
Yoon Hee-Ku <heegu0311@naver.com>
2021-02-17 17:26:59 +0900

    20. useReducer (useReducer 사용)

--- wrs/src/App.js
+++ wrs/src/App.js
@@ -1,13 +1,5 @@
-// github 테스트
-
-import Counter from './Counter';
-// import Hello from './Hello';
-// import InputSample from './InputSample';
-// import InputSampleOneInput from './InputSampleOneInput';
-import { useRef, useState, useMemo, useCallback } from 'react';
 import CreateUser from './CreateUser';
 import UserList from './UserList';
-// import Wrapper from './Wrapper';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
@@ -16,97 +8,41 @@ function countActiveUsers(users) {
   return a.length;
 }
 
-function App() {
-
-  const [inputs, setInputs] = useState({
+const initialState = {
+  inputs : {
     username : '',
     email : ''
-  });
+  },
+
+  users: [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false
+    }
+  ]
+}
+
+function App() {
   
-  const {username, email} = inputs;
-
-  const onChange = useCallback(
-    (e) => {
-    const {name, value} = e.target;
-    setInputs({
-      ...inputs,
-      [name] : value
-    });
-  },[inputs]);
-
-  const [users, setUsers] = useState([
-    {
-        id : 1,
-        username : 'A',
-        email : "A@gmail.com",
-        active : true
-    },
-    {
-        id : 2,
-        username : 'B',
-        email : "B@gmail.com",
-        active : false
-    },
-    {
-        id : 3,
-        username : 'C',
-        email : "C@gmail.com",
-        active : false
-    }
-  ]);
-  let nextId = useRef(4);
-  const onCreate = useCallback(() => {
-    const user = {
-      id : nextId.current,
-      username,
-      email 
-    }
-    setUsers([  // users 는 배열이므로 대괄호 사용
-      ...users, user
-    ]);
-
-    // setUsers(users.concat(user)); // 바로 위 setUsers와 동일할 기능. 다른 방법
-
-
-    setInputs({
-      username: '',
-      email: ''
-    });
-
-    nextId.current+=1;
-  },[users, username, email]);
-
-  const onRemove = useCallback((id) => {
-    setUsers(users.filter(user => user.id !== id ));
-  }, [users])
-
-  const onToggle = useCallback((id) => {
-    setUsers(
-      users.map(user =>
-        user.id === id ? { ...user, active: !user.active } : user
-      )
-    );
-  },[users]);
-
-  const count = useMemo(() => countActiveUsers(users), [users]);
-
   return (
     <div>
-        {/* <Wrapper>
-        <Hello name="react" color="green" isSpecial={true}/>
-        <Hello color="pink"/>
-        <b>7. useState 를 통해 컴포넌트에서 바뀌는 값 관리하기</b> */}
-        <Counter></Counter>
-        {/* <InputSample></InputSample>
-        <InputSampleOneInput></InputSampleOneInput> */}
-        <CreateUser
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-        />
-        <UserList users={users} onRemove={onRemove} onToggle={onToggle}></UserList>
-        <div>활성사용자 수 : {count}</div>
+       <CreateUser />
+        <UserList users={[]}></UserList>
+        <div>활성사용자 수 : 0 </div>
       {/* </Wrapper> */}
     </div>
   );
