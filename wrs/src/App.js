3576d98e97a7dcc2eac10ab5e6994d0d914135df
Yoon Hee-Ku <heegu0311@naver.com>
2021-02-17 17:04:40 +0900

    20. useReducer (Counter)

--- wrs/src/App.js
+++ wrs/src/App.js
@@ -1,6 +1,6 @@
 // github 테스트
 
-// import Counter from './Counter';
+import Counter from './Counter';
 // import Hello from './Hello';
 // import InputSample from './InputSample';
 // import InputSampleOneInput from './InputSampleOneInput';
@@ -92,12 +92,12 @@ function App() {
 
   return (
     <div>
-      {/* <Wrapper>
+        {/* <Wrapper>
         <Hello name="react" color="green" isSpecial={true}/>
         <Hello color="pink"/>
-        <b>7. useState 를 통해 컴포넌트에서 바뀌는 값 관리하기</b>
+        <b>7. useState 를 통해 컴포넌트에서 바뀌는 값 관리하기</b> */}
         <Counter></Counter>
-        <InputSample></InputSample>
+        {/* <InputSample></InputSample>
         <InputSampleOneInput></InputSampleOneInput> */}
         <CreateUser
         username={username}
